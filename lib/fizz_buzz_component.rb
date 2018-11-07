5c6637608d7f114cc115dc74358d56776dfdacb4
Nathan Ladd <nathanladd+github@gmail.com>
2018-11-07 12:01:54 -0600

    Initiated event is removed

--- lib/fizz_buzz_component.rb
+++ lib/fizz_buzz_component.rb
@@ -3,7 +3,6 @@ require 'try'
 
 require 'fizz_buzz_component/game'
 
-require 'fizz_buzz_component/messages/events/initiated'
 require 'fizz_buzz_component/messages/events/answered'
 require 'fizz_buzz_component/messages/events/answer_rejected'
 require 'fizz_buzz_component/messages/events/finished'
--- lib/fizz_buzz_component/controls.rb
+++ lib/fizz_buzz_component/controls.rb
@@ -12,7 +12,6 @@ require 'fizz_buzz_component/controls/counter'
 
 require 'fizz_buzz_component/controls/game'
 
-require 'fizz_buzz_component/controls/events/initiated'
 require 'fizz_buzz_component/controls/events/answered'
 require 'fizz_buzz_component/controls/events/answer_rejected'
 require 'fizz_buzz_component/controls/events/finished'
